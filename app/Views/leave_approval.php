7aa8c7d9753cfce130ca85383948be79eacee4c5
Leo-Bikuri <leo.bikuri@strathmore.edu>
2021-10-19 16:48:50 +0300

    4th commit

--- app/Views/leave_approval.php
+++ app/Views/leave_approval.php
@@ -1,48 +1,59 @@
-<?php include ('sidemenu.php')?>
+<?php include('sidemenu.php')?>
 <!doctype html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Leave Approval</title>
-    <link rel="stylesheet" href="/css/leave_approval.css">
-    
+<meta charset="UTF-8">
+<meta name="viewport"
+      content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+<meta http-equiv="X-UA-Compatible" content="ie=edge">
+<title>Register</title>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+<link rel="stylesheet" href="/css/leave_approval.css">
 </head>
 <body>
+    <div class="position-absolute form">
+        <h1 class="text-dark">Leave Approval</h1>
+            <form class="row g-4" style="max-height: 600px" method="post" action="/register">
+                <div class="row">
+                    <div class="col-12">
+                        <label for="staffid" class="text-dark form-label">Staff Number:</label>
+                        <input type="text" class="form-control" id="staffid" name="staff_number" placeholder="" size="20" readonly>
+                    </div>
+                    <div class="col-md-6">
+                        <label for="name" class=" text-dark mt-2 form-label">Name</label>
+                        <input type="text" class="form-control" id="name" name="name" placeholder="" readonly>
+                    </div>
 
-    <form method="post" action="adminmethods.php">
-    <div class="details employee">
-        <div class="leaveDetails">
-            <div class="cardHeader">
-                <h2>Leave Approval</h2>
-            </div>
-            <label for="staff_number">Staff number:</label> 
-            <input type="text" placeholder="" name="staff_number" class="admin_input" id="staff_number" value="Demo" readonly></br>
-           <label for="name">Name: </label>
-           <input type="text" placeholder="" name="name" class="admin_input" id="name" value="Demo"readonly></br>
-           <label for="department">Department:</label>
-           <input type="text" placeholder="" name="department" class="admin_input" id="department"  value="Demo"readonly></br>
-           <label for="leave_type">Leave type:</label>
-           <input type="text" placeholder="" name="leave_type" class='admin_input' id="leave_type" value="Demo" readonly></br>
-           <label for="start_date">Start date:</label>
-           <input type="text" placeholder="" name="start_date" class='admin_input' id="start_date" value="Demo" readonly>
-           <label for="end_date">End date:</label>
-           <input type="text" placeholder="" name="end_date" class='admin_input' id="end_date" value="Demo" readonly></br>
-           <label for="duration">Duration:</label>
-           <input type="text" placeholder="" name="duration" class='admin_input' id="duration" value="Demo" readonly></br>
-
-
-
-            <div class="button">
-           <input class="btn approve" type="submit" name="approve" value="Approve"> 
-           <input class="btn deny" type="submit" name="deny" value="Deny">
-           </div>
-
-        </div>
-    </div>
-</form>
+                    <div class="col-md-6">
+                        <label for="department" class="text-dark mt-2 form-label">Department</label>
+                        <input type="text" class="form-control" id="department"  name="department" placeholder="" readonly>
+                    </div>
+                    <div class="col-md-6">
+                        <label for="leave_type" class="text-dark mt-2 form-label">Leave type</label>
+                        <input type="text" class="form-control" id="leave_type" name="leave_type" placeholder="" readonly>
+                    </div>
+                    <div class="col-md-6">
+                        <label for="duration" class="text-dark mt-2 form-label">Duration</label>
+                        <input type="text"  class="form-control" id="duration" name="duration" value="demo" readonly>
+                    </div>
+                    <div class="col-md-6">
+                        <label for="department" class="text-dark mt-2 form-label">Start Date</label>
+                        <input type="text"  class="form-control" id="start_date" name="start_date" value="demo" readonly>
+                    </div>
+                    <div class="col-md-6">
+                        <label for="usertype" class="text-dark mt-2 form-label">End Date</label>
+                        <input type="text"  class="form-control" id="end_date" name="end_date" value="demo" readonly>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-12 ml-5 col-sm8- offset-sm-2 offset-md-4 pt-4 from-wrapper">
+                        <div class="button">
+                    <input class="btn approve" type="submit" name="approve" value="Approve"> 
+                    <input class="btn deny" type="submit" name="deny" value="Deny">
+                    </div>
+                    </div>
+                </div>
+            </form>
     </div>
 </body>
 </html>
