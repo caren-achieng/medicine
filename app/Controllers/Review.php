34fe9a1a5572f5eefd878b6c13c573b3f23a9f02
Leo-Bikuri <leo.bikuri@strathmore.edu>
2021-12-05 22:56:53 +0300

    New admin dashboard

---
+++ app/Controllers/Review.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Controllers;
+
+class Review extends BaseController
+{
+    public function index()
+    {
+        echo view('review');
+
+    }
+}
---
+++ app/Models/LeaveModel.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+use CodeIgniter\Model;
+class LeaveModel extends Model{
+
+    protected $table = 'leaves';
+    protected $allowedFields = [
+        'staff_number',
+        'leave_type',
+        'start_date',
+        'end_date',
+        'leave_status',
+        'approver_id'
+    ];
+}
