4bfebcdc423ef2fcfcd07e2d56e1b3cc5b2706ac
Leo-Bikuri <leo.bikuri@strathmore.edu>
2021-12-06 00:47:57 +0300

    got css working

--- app/Views/admin/users.php
+++ app/Views/admin/users.php
@@ -14,7 +14,7 @@
   <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
   <!-- CSS Files -->
   <link href="/css/bootstrap.min.css" rel="stylesheet" />
-  <link href="/css/now-ui-dashboard.css?v=1.5.0" rel="stylesheet" />
+  <link href="/css/dashboard.css?v=1.5.0" rel="stylesheet" />
 </head>
 
 <body class="">
