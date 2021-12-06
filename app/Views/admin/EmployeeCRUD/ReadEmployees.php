2a2aeb7116125cc0b4b0579a9c9cd13f459c0539
caren-achieng <caren.achieng@strathmore.edu>
2021-12-06 19:17:27 +0300

    CRUD Functionality Employee

--- app/Views/admin/EmployeeCRUD/ReadEmployees.php
+++ app/Views/admin/EmployeeCRUD/ReadEmployees.php
@@ -23,90 +23,102 @@
     </style>
 </head>
 <body>
-<div class="wrapper">
+<div class="wrapper ">
     <div class="main-panel" id="main-panel">
+        <!-- Navbar -->
         <nav class="navbar navbar-expand-lg navbar-transparent  bg-primary  navbar-absolute">
             <div class="container-fluid">
-                <div class="collapse navbar-collapse justify-content-end" id="navigation"></div>
+                <div class="navbar-wrapper">
+                    <div class="navbar-toggle">
+                    </div>
+                    <a class="navbar-brand">View Employees</a>
+                </div>
+                <div class="collapse navbar-collapse justify-content-end" id="navigation">
+                </div>
             </div>
         </nav>
-        <!--    <div class="container form position-relative ">-->
-
-        <div class="panel-header panel-header-sm"></div>
+        <!-- End Navbar -->
+        <div class="panel-header panel-header-sm">
+        </div>
         <div class="content">
-            <div class="col-md-12">
-                <div class="card ">
-                    <div class="card-body">
-                        <div class="table-responsive">
-                            <table class="table table-dark table-striped table-bordered">
-                                <thead>
-                                <tr>
-                                    <th scope="col">Staff Number</th>
-                                    <th scope="col">ID/Passport</th>
-                                    <th scope="col">Title</th>
-                                    <th scope="col">First Name</th>
-                                    <th scope="col">Middle Name</th>
-                                    <th scope="col">Last Name</th>
-                                    <th scope="col">DOB</th>
-                                    <th scope="col">Gender</th>
-                                    <th scope="col">Email</th>
-                                    <th scope="col">Marital Status</th>
-                                    <th scope="col">Religion</th>
-                                    <th scope="col">Mobile Number</th>
-                                    <th scope="col">Home Tel</th>
-                                    <th scope="col">KRA PIN Num</th>
-                                    <th scope="col">NHIF PIN Num</th>
-                                    <th scope="col">NSSF PIN Num</th>
-                                    <th scope="col">Postal Address</th>
-                                    <th scope="col">Post Code</th>
-                                    <th scope="col">Residence</th>
-                                    <th scope="col">Town</th>
-                                    <th scope="col">Home County</th>
-                                    <th scope="col">Department</th>
-                                    <th scope="col">Action</th>
-                                </tr>
-                                </thead>
-                                <tbody>
-                                <?php
-                                foreach($employees as $row) : ?>
-                                    <tr>
-                                        <td><?= $row['staff_number'] ?></td>
-                                        <td><?= $row['id/passport'] ?></td>
-                                        <td><?= $row['title'] ?></td>
-                                        <td><?= $row['fname'] ?></td>
-                                        <td><?= $row['mname'] ?></td>
-                                        <td><?= $row['lname'] ?></td>
-                                        <td><?= $row['dob'] ?></td>
-                                        <td><?= $row['gender'] ?></td>
-                                        <td><?= $row['email'] ?></td>
-                                        <td><?= $row['maritalstatus'] ?></td>
-                                        <td><?= $row['religion'] ?></td>
-                                        <td><?= $row['mobilenum'] ?></td>
-                                        <td><?= $row['hometel'] ?></td>
-                                        <td><?= $row['krapin'] ?></td>
-                                        <td><?= $row['nhifpin'] ?></td>
-                                        <td><?= $row['nssfpin'] ?></td>
-                                        <td><?= $row['postaddress'] ?></td>
-                                        <td><?= $row['postcode'] ?></td>
-                                        <td><?= $row['residence'] ?></td>
-                                        <td><?= $row['town'] ?></td>
-                                        <td><?= $row['county'] ?></td>
-                                        <td><?= $row['department'] ?></td>
-                                        <td>
-                                            <a href="<?= base_url('ReadEmployees/UpdateEmployees/'.$row['staff_number'])?>" class="btn btn-success btn-sm">Edit</a>
-                                            <a href="<?= base_url('ReadEmployees/DeleteEmployees/'.$row['staff_number'])?>" class="btn btn-danger btn-sm">Delete</a>
-                                        </td>
-                                    </tr>
-                                <?php endforeach; ?>
-                                </tbody>
-                            </table>
-                            <?php echo $pager->links(); ?>
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="card">
+                        <div class="card-header">
+                            <h4 class="card-title">Employees</h4>
                         </div>
+                        <div class="card-body">
+                            <div class="table-responsive">
+                                <table class="table">
+                                    <thead class=" text-primary">
+                                    <th>Staff Number</th>
+                                    <th>ID/Passport</th>
+                                    <th>Title</th>
+                                    <th>First Name</th>
+                                    <th>Middle Name</th>
+                                    <th>Last Name</th>
+                                    <th>DOB</th>
+                                    <th>Gender</th>
+                                    <th>Email</th>
+                                    <th>Marital Status</th>
+                                    <th>Religion</th>
+                                    <th>Mobile Number</th>
+                                    <th>Home Tel</th>
+                                    <th>KRA PIN Num</th>
+                                    <th>NHIF PIN Num</th>
+                                    <th>NSSF PIN Num</th>
+                                    <th>Postal Address</th>
+                                    <th>Post Code</th>
+                                    <th>Residence</th>
+                                    <th>Town</th>
+                                    <th>Home County</th>
+                                    <th>Department</th>
+                                    <th>Action</th>
+                                    </thead>
+                                    <tbody>
+                                    <?php
+                                    foreach($employees as $row) : ?>
+                                        <tr>
+                                            <td><?= $row['staff_number'] ?></td>
+                                            <td><?= $row['id/passport'] ?></td>
+                                            <td><?= $row['title'] ?></td>
+                                            <td><?= $row['fname'] ?></td>
+                                            <td><?= $row['mname'] ?></td>
+                                            <td><?= $row['lname'] ?></td>
+                                            <td><?= $row['dob'] ?></td>
+                                            <td><?= $row['gender'] ?></td>
+                                            <td><?= $row['email'] ?></td>
+                                            <td><?= $row['maritalstatus'] ?></td>
+                                            <td><?= $row['religion'] ?></td>
+                                            <td><?= $row['mobilenum'] ?></td>
+                                            <td><?= $row['hometel'] ?></td>
+                                            <td><?= $row['krapin'] ?></td>
+                                            <td><?= $row['nhifpin'] ?></td>
+                                            <td><?= $row['nssfpin'] ?></td>
+                                            <td><?= $row['postaddress'] ?></td>
+                                            <td><?= $row['postcode'] ?></td>
+                                            <td><?= $row['residence'] ?></td>
+                                            <td><?= $row['town'] ?></td>
+                                            <td><?= $row['county'] ?></td>
+                                            <td><?= $row['department'] ?></td>
+                                            <td>
+                                                <a href="<?= base_url('ReadEmployees/UpdateEmployees/'.$row['staff_number'])?>" class="btn btn-success btn-sm">Edit</a>
+                                                <a href="<?= base_url('ReadEmployees/DeleteEmployees/'.$row['staff_number'])?>" class="btn btn-danger btn-sm">Delete</a>
+                                            </td>
+                                        </tr>
+                                    <?php endforeach; ?>
+                                    </tbody>
+                                </table>
+                                <?php echo $pager->links(); ?>
+                            </div>
+                        </div>
+
                     </div>
                 </div>
             </div>
         </div>
     </div>
 </div>
+
 </body>
 </html>
