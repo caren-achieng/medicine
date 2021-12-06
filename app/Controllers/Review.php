f4c6251c6e2afeb974115994ec4ea561346368a2
Leo-Bikuri <leo.bikuri@strathmore.edu>
2021-12-06 21:43:06 +0300

    Added Review files

--- app/Controllers/Review.php
+++ app/Controllers/Review.php
@@ -2,10 +2,26 @@
 
 namespace App\Controllers;
 
+use App\Models\UserModel;
+
 class Review extends BaseController
 {
     public function index()
     {
         echo view('admin/review');
+
+    }
+
+    public function viewUsers(int $role = null)
+    {
+        $users = new UserModel();
+
+        $allUsers = $users->getUsers();
+
+        if ($role !== false)
+            $allUsers = $users->getUsers($role);
+
+
+        return $this->response->setJSON($allUsers);
     }
 }
