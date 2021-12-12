eda3c70a8a5e20e8e7ce04d41901b8731a7bd1dc
CodeWith-PeterBull <77413110+CodeWith-PeterBull@users.noreply.github.com>
2021-12-12 23:09:01 +0300

    Update LeaveReview.php

--- app/Controllers/LeaveReview.php
+++ app/Controllers/LeaveReview.php
@@ -13,7 +13,7 @@ class LeaveReview extends BaseController
         helper (['form', 'url']);
         $users=new UserModel();
         $leavesApplied= new Review();
-        $data1['leavesPendingData'] = json_decode(json_encode($leavesApplied->join('users','users.staff_number=leaves.staff_number')->WHERE('leave_status=',"")->findAll()),true);
+        $data1['leavesPendingData'] = json_decode(json_encode($leavesApplied->join('users','users.staff_number=leaves.staff_number')->WHERE('leave_status=',"Pending")->findAll()),true);
 
         return view('admin/review',$data1);
     }
